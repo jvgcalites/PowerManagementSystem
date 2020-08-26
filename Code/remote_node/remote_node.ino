c640276e84841db87be05e01bb5536e7385e594e
John Vincent Calites <41093832+jvgcalites@users.noreply.github.com>
2020-08-26 16:51:35 +0800

    fixed bug remote node

--- Code/remote_node/remote_node.ino
+++ Code/remote_node/remote_node.ino
@@ -15,10 +15,6 @@ RF24 radio(9, 10); //create object to control and communicate with nRF24L01
 const int ledPin = 2; //LED on digital pin 2
 byte payload = 1; //payload to sent to actuator nodes
 
-// INTERRUPT STATUS
-volatile int previousState = 1;
-volatile int currentState = 0; 
-
 void setup() {
     // setup NRF24L01
     SPI.begin(); //start SPI communication
@@ -37,8 +33,7 @@ void setup() {
 }
 
 void wakeUpNow(){
-    // change motion sensor state from ON to OFF and vice versa.
-    currentState = !previousState;
+    blinkLed();
 }
 
 void hibernate(){
@@ -46,11 +41,11 @@ void hibernate(){
     ADCSRA &= ~(1 << 7); // disable ADC
     sleep_enable();
     sleep_bod_disable();
-    attachPCINT(19, wakeUpNow, CHANGE);
-    attachPCINT(20, wakeUpNow, CHANGE);
-    attachPCINT(22, wakeUpNow, CHANGE);
-    attachPCINT(23, wakeUpNow, CHANGE);
-    attachPCINT(0, wakeUpNow, CHANGE);
+    attachPCINT(19, wakeUpNow, RISING);
+    attachPCINT(20, wakeUpNow, RISING);
+    attachPCINT(22, wakeUpNow, RISING);
+    attachPCINT(23, wakeUpNow, RISING);
+    attachPCINT(0, wakeUpNow, RISING);
     sleep_mode();
     sleep_disable();
     //detachPCINT(PCINT_DCPIN4);
@@ -63,35 +58,30 @@ void loop() {
         radio.powerUp();
         radio.write(&payload, 1);
         radio.powerDown();
-        blinkLed();
     }
     else if(digitalRead(4) == LOW){
         payload = 2;
         radio.powerUp();
         radio.write(&payload, 1);
         radio.powerDown();
-        blinkLed();
     }
     else if(digitalRead(6) == LOW){
         payload = 3;
         radio.powerUp();
         radio.write(&payload, 1);
         radio.powerDown();
-        blinkLed();
     }
     else if(digitalRead(7) == LOW){
         payload = 4;
         radio.powerUp();
         radio.write(&payload, 1);
         radio.powerDown();
-        blinkLed();
     }
     else if(digitalRead(8) == LOW){
         payload = 5;
         radio.powerUp();
         radio.write(&payload, 1);
         radio.powerDown();
-        blinkLed();
     }
     else{
         // It should not reach here, in theory
